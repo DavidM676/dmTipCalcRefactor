a15b9f17eb1ecab0866a5c5dfab7bed7968e361d
student <e@nycstudents.net>
2022-10-06 12:31:42 -0400

    initial commit

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -1,5 +1,5 @@
 
-
+//
 public class TipCalculator {
 
     int numPeople;
--- src/TipCalculatorRunner.java
+++ src/TipCalculatorRunner.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 import java.text.DecimalFormat;
-
+//
 public class TipCalculatorRunner {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
